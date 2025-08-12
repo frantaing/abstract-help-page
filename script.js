78e4ffbb1097056edf98c645f53e33a5c4ec9d7b
frantan <francinetaing13@gmail.com>
2025-08-12 21:02:01 +0700

    + javascript for mobile-menu-search

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ hamburgerButton.addEventListener('click', () => {
   hamburgerButton.classList.toggle('is-active');
     mobileMenu.classList.toggle('is-active');
 });
+
+// MOBILE MENU SEARCH
+const mobileMenuSearchButton = document.querySelector('.mobile-menu .menu-search');
+const mobileMenuSearchContainer = document.querySelector('.mobile-menu-search');
+const closeMobileMenuSearchButton = document.querySelector('.mobile-menu-search-close');
+
+mobileMenuSearchButton.addEventListener('click', () => {
+    mobileMenuSearchContainer.style.display = 'flex';
+});
+
+closeMobileMenuSearchButton.addEventListener('click', () => {
+    mobileMenuSearchContainer.style.display = 'none';
+});
